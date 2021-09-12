d977fc62af1ff6a410c1d358aeb49801c991ee39
xw <myxw94@gmail.com>
2021-09-12 22:59:58 +0800

    更新断电3次进入配网模式

--- boot.py
+++ boot.py
@@ -2,12 +2,17 @@
 
 
 import uos, machine,time,lib
+from httpServer import http
 import gc
 import webrepl
 webrepl.start()
 gc.collect()
+
+
+//
+
 #响应按键开关
-device="MPY_Light"
+device="TEST8266"
 power=lib.flashLed(5)#开关控制引脚
 p4=lib.flashLed(4)
 p12=lib.flashLed(12)
@@ -22,41 +27,70 @@ def led(c):
     p4.sw(0);p12.sw(0)
 led("blue")
 def sw():
+
     power.sw()
     led("red") if power.sw('') else led("blue")
+
 btn.click(sw)
+
 #响应按键开关
+
+def setFile(s=1):
+      file="isRset.py"
+      num=lib.file(file)
+      if type(s).__name__=="Timer" or s==1 or s=='r':
+        if num:
+          if s=='r':
+            return int(num)
+          num=int(num)
+          lib.file(file,str(num+1))
+          return num+1
+        else:
+          if s=='r':
+            return False         
+          lib.file(file,"1")
+      if s==0:
+        lib.file(file,"1")
+        return 1
+      if s==4:
+        lib.file(file,"4")
+        return 4  
+
+
 def _reset(s=1):
-    lib.file("isRset.py","4")
+    setFile(4)
     machine.reset()
     
 btn.press(_reset,5000)
+def wifi_setup(url):
+        if url =="/":
+          serv.sendall('<form action="wifi">SSD:<br><input type="text" name="ssd" value=""><br>PASSWORD<br><input type="text" name="pwd" value=""><hr>KEY<br><input type="text" name="key" value=""><input type="submit" value="Submit"></form> ')
+          serv.sendall('<hr/>')
+          ap_list=lib.wifi()[0].scan()
+          for i in ap_list:
+            serv.sendall("%s ,%d<br/>"%(i[0].decode(),i[3]))
+        if url.find("/wifi")!=-1:
+            d=(serv.get_Args(url))
+            print(d)
+            if d.get("ssd") !=None and d.get("pwd")!=None:
+              conf="ssd='%s'\r\npwd='%s' \r\nkey='%s'"%(d.get("ssd"),d.get("pwd"),d.get("key"))
+              lib.file(_cfg,conf)
+              serv.send("\r\n") 
+              serv.send("设置成功，即将重启。")
+              time.sleep(3)
+              machine.reset()
+        else:
+         serv.send("666 \r\n")
+
 try:
-  if int(lib.file("isRset.py")) >=3:
-      from httpServer import http
-      def wifi_setup(url):
-              if url =="/":
-                serv.sendall('<form action="wifi">SSD:<br><input type="text" name="ssd" value=""><br>PASSWORD<br><input type="text" name="pwd" value=""><hr>KEY<br><input type="text" name="key" value=""><input type="submit" value="Submit"></form> ')
-                serv.sendall('<hr/>')
-                ap_list=lib.wifi()[0].scan()
-                for i in ap_list:
-                  serv.sendall("%s ,%d<br/>"%(i[0].decode(),i[3]))
-              if url.find("/wifi")!=-1:
-                  d=(serv.get_Args(url))
-                  print(d)
-                  if d.get("ssd") !=None and d.get("pwd")!=None:
-                    conf="ssd='%s'\r\npwd='%s' \r\nkey='%s'"%(d.get("ssd"),d.get("pwd"),d.get("key"))
-                    lib.file(_cfg,conf)
-                    serv.send("\r\n") 
-                    serv.send("设置成功，即将重启。")
-                    time.sleep(3)
-                    machine.reset()
-              else:
-               serv.send("666 \r\n")    
+  #print("重启次数：",setFile("read"))
+  #tim=machine.Timer(-1)     
+  #tim.init(period=5000, mode=machine.Timer.ONE_SHOT, callback=setFile)
+  if setFile("r") >=3:
       p4.flash(100)
       p12.flash(50)
       lib.ap(device)
-      lib.file("isRset.py","0")
+      setFile(0)
       serv=http("0.0.0.0",80)
       while 1:
         serv.http(wifi_setup)    
